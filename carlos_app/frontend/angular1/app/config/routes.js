a59ae44eb9e908568c6a2d97ac897cb32d3b3e79
António Carlos Sousa <antonio_carlos_alves_sousa@hotmail.com>
2018-07-25 23:24:05 +0100

    start work with ui router

---
+++ carlos_app/frontend/angular1/app/config/routes.js
@@ -0,0 +1,15 @@
+/**  */
+angular.module('primeiraApp').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('dashboard', {
+            url: '',
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            templateUrl: "billingCycle/tabs.html"
+        });
+
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+]);
